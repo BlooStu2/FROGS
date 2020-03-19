b6c0c0cc673807c18ded3f75f81125566f56ab8e
BlooStu2 <abrough.brough232@gmail.com>
2020-03-19 22:16:29 +0000

    the game is working but very buggy

--- Board.java
+++ Board.java
@@ -1,14 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Board{
+public class Board implements ActionListener{
     private JFrame frame = new JFrame("FROGS");
     private Square[][] buttList = new Square[5][5];
     private String[][] levels = new String[40][];
     private JPanel panel = new JPanel();
     private GridLayout layout = new GridLayout(5, 5);
-    //private String levels = {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
     private int lilyswitch = 0;
+    private boolean movemode = false;
+    private Square selectsquare;
+    private int level = 1;
 
     public Board(){
         frame.setSize(750,750);
@@ -30,63 +33,156 @@ public class Board{
         levels[14] = new String[] {"g","g","l","l","l","l","l","g","r","g","l","l","l"};
         levels[15] = new String[] {"l","l","r","l","g","l","l","g","l","l","l","l","l"};
         levels[16] = new String[] {"l","r","l","l","l","l","g","l","g","g","l","g","l"};
-        levels[17] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[18] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[19] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[20] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[21] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[22] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[23] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[24] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[25] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[26] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[27] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[28] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[29] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[30] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[31] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[32] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[33] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[34] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[35] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[36] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[37] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[38] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
-        levels[39] = new String[] {"l","l","l","l","l","l","l","l","l","l","l","l","l"};
+        levels[17] = new String[] {"l","g","l","l","l","g","g","g","g","g","l","r","l"};
+        levels[18] = new String[] {"g","g","l","l","g","r","l","l","g","g","l","g","l"};
+        levels[19] = new String[] {"l","g","g","g","g","l","l","g","r","g","l","l","l"};
+        levels[20] = new String[] {"g","l","l","g","r","l","g","l","l","g","l","l","g"};
+        levels[21] = new String[] {"l","l","l","g","g","l","g","l","l","l","g","r","g"};
+        levels[22] = new String[] {"l","l","g","g","g","l","g","l","g","l","g","l","r"};
+        levels[23] = new String[] {"l","g","l","g","g","g","g","l","g","l","l","l","r"};
+        levels[24] = new String[] {"l","l","l","g","g","l","l","g","g","l","g","l","r"};
+        levels[25] = new String[] {"r","g","l","g","l","g","g","l","l","g","l","l","l"};
+        levels[26] = new String[] {"l","l","r","l","l","g","l","l","g","l","g","g","l"};
+        levels[27] = new String[] {"l","r","l","g","g","g","l","g","l","l","g","l","g"};
+        levels[28] = new String[] {"l","r","l","l","l","l","g","l","g","g","g","l","g"};
+        levels[29] = new String[] {"g","g","l","g","g","g","l","l","g","l","l","l","r"};
+        levels[30] = new String[] {"l","g","r","g","l","l","g","l","l","g","l","l","l"};
+        levels[31] = new String[] {"l","l","l","g","l","g","g","l","g","g","r","g","l"};
+        levels[32] = new String[] {"l","l","l","l","l","g","r","g","g","g","l","g","l"};
+        levels[33] = new String[] {"l","g","l","l","l","l","l","l","g","g","g","g","r"};
+        levels[34] = new String[] {"g","l","g","g","g","l","l","l","g","g","l","l","r"};
+        levels[35] = new String[] {"l","g","l","g","g","l","l","l","g","g","g","l","r"};
+        levels[36] = new String[] {"l","l","l","g","l","g","g","l","l","g","r","g","l"};
+        levels[37] = new String[] {"g","g","l","l","g","l","l","l","g","l","l","g","r"};
+        levels[38] = new String[] {"r","l","g","l","g","g","g","l","g","l","l","g","l"};
+        levels[39] = new String[] {"l","l","g","g","l","g","g","l","l","g","r","g","l"};
 
-     for(int i = 0; i<5; i++){      //eventually change i++ to i+=2 to remove lilyswitch
+        for(int i = 0; i<5; i++){      //eventually change i++ to i+=2 to remove lilyswitch
             for(int j = 0; j<5; j++){
                 if (lilyswitch == 0){
                     buttList[i][j] = new Square();
+                    buttList[i][j].addActionListener(this);
                     lilyswitch = 1;
                 }
                 else {
                     lilyswitch = 0;
                     buttList[i][j] = new Square();
+                    buttList[i][j].addActionListener(this);
                 }
-                panel.add(buttList[i][j].b);
+                panel.add(buttList[i][j]);
             }
         }
         
-        this.setLevel(1);
+        this.setLevel(level);
         frame.setContentPane(panel);
         frame.setVisible(true);
     }
     public void setLevel(int levelnum){
+        lilyswitch = 0;
         int index = 0;
         for(int i = 0; i<5; i++){
             for(int j = 0; j<5; j++){
                 if (lilyswitch == 0){
                     buttList[i][j].Change(levels[levelnum-1][index]);
-                    buttList[i][j].Update();
                     lilyswitch = 1;
                     index++;
                 }
                 else {
                     lilyswitch = 0;
                 }
-                panel.add(buttList[i][j].b);
+                panel.add(buttList[i][j]);
             }
         }
+    }
+    public void actionPerformed(ActionEvent e){
+         if(e.getSource() instanceof Square){
+            Square pressedSquare = (Square)e.getSource();
+            if (movemode == false){
+                if(pressedSquare.getType().equals("g")){
+                    pressedSquare.Change("gg");
+                    movemode = true;
+                    selectsquare = pressedSquare;
+                }
+                else if(pressedSquare.getType().equals("r")){
+                    pressedSquare.Change("rg");
+                    movemode = true;
+                    selectsquare = pressedSquare;
+                }
+            }
+            else{
+                int newi = this.getSquare(pressedSquare)[0];
+                int oldi = this.getSquare(selectsquare)[0];
+                int newj = this.getSquare(pressedSquare)[1];
+                int oldj = this.getSquare(selectsquare)[1];
+                if(pressedSquare.getType().equals("l")){
+                    if((Math.abs(newi - oldi) == 2) && (Math.abs(newj - oldj) == 2)){
+                        if((buttList[(newi+oldi)/2][(newj+oldj)/2].getType().equals("g"))){
+                            if(selectsquare.getType().equals("gg")){
+                                pressedSquare.Change("g");
+                            }
+                            else{
+                                pressedSquare.Change("r");
+                            }
+                            selectsquare.Change("l");
+                            buttList[(newi+oldi)/2][(newj+oldj)/2].Change("l");
+                            this.checkLevel();
+                        }
+                        else if((buttList[(newi+oldi)/2][(newj+oldj)/2].getType().equals("r"))){
+                            pressedSquare.Change("g");
+                            selectsquare.Change("l");
+                            buttList[(newi+oldi)/2][(newj+oldj)/2].Change("l");
+                            this.gameOver();
+                        }
+                    }
+                    else if ((Math.abs(newi - oldi) == 1) && (Math.abs(newj - oldj) == 1)){
+                        if((buttList[newi][newj].getType().equals("l"))){
+                            if(selectsquare.getType().equals("gg")){
+                                pressedSquare.Change("g");
+                            }
+                            else{
+                                pressedSquare.Change("r");
+                            }
+                            buttList[oldi][oldj].Change("l");
+                            System.out.println(selectsquare.getType());
+                        }
+                    }
+                    else{
+                        if(selectsquare.getType().equals("gg")){
+                            pressedSquare.Change("g");
+                        }
+                        else{
+                            pressedSquare.Change("r");
+                        }
+                    }
+                }
+                movemode = false;
+            }
+        }
+    }
+    public int[] getSquare(Square s){
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(buttList[i][j] == s){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+    public void gameOver(){
+        System.out.println("Game Over!");
+    }
+
+    public void checkLevel(){
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(buttList[i][j].getType().equals("g")){
+                    System.out.println("check");
+                    return;
+                }
+            }
+        }
+        level++;
+        this.setLevel(level);
     }
 }
--- Square.java
+++ Square.java
@@ -1,59 +1,41 @@
 import javax.swing.*;
 
-public class Square{
+public class Square extends JButton{
     private ImageIcon w = new ImageIcon("./assets/Water.png");
     private ImageIcon l = new ImageIcon("./assets/LilyPad.png");
     private ImageIcon r = new ImageIcon("./assets/RedFrog.png");
     private ImageIcon rg = new ImageIcon("./assets/RedFrog2.png");
     private ImageIcon g = new ImageIcon("./assets/GreenFrog.png");
     private ImageIcon gg = new ImageIcon("./assets/GreenFrog2.png");
-    public JButton b;
     private String type = "w";
 
     public Square(){
-        b = new JButton(w);
+        this.setIcon(w);
     }
     public Square(String yeet){
-        type = yeet;
-        b = new JButton(yeet);
-        if (type.equals("l")){
-            b = new JButton(l);
-        }
-        else if (type.equals("r")){
-            b = new JButton(r);
-        }
-        else if (type.equals("g")){
-            b = new JButton(g);
-        }
-        else if (type.equals("rg")){
-            b = new JButton(rg);
-        }
-        else if (type.equals("gg")){
-            b = new JButton(gg);
-        }
+        this.Change(yeet);
     }
-    public void Update(){
-        if(type.equals("w")){
-            b.setIcon(w);
-        }
-        else if(type.equals("l")){
-            b.setIcon(l);
-        }
-        else if(type.equals("r")){
-            b.setIcon(r);
-        }
-        else if(type.equals("rg")){
-            b.setIcon(rg);
-        }
-        else if(type.equals("g")){
-            b.setIcon(g);
-        }
-        else if(type.equals("gg")){
-            b.setIcon(gg);
-        }
-    }
+
     public void Change(String item){
         type = item;
+        if(type.equals("w")){
+            this.setIcon(w);
+        }
+        else if(type.equals("l")){
+            this.setIcon(l);
+        }
+        else if(type.equals("r")){
+            this.setIcon(r);
+        }
+        else if(type.equals("rg")){
+            this.setIcon(rg);
+        }
+        else if(type.equals("g")){
+            this.setIcon(g);
+        }
+        else if(type.equals("gg")){
+            this.setIcon(gg);
+        }
     }
     public String getType(){
         return type;
